6a67673e2672883610a0066a9b918d49e86fe8bf
Fabiana Limma <fabianallima@hotmail.com>
2022-12-06 16:13:33 -0300

    layout da home do app

--- CloneSpotify/ViewController.swift
+++ CloneSpotify/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var subscribe: UIButton!
+
+    @IBOutlet weak var entrar: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let view = UIView()
+        view.layer.borderColor = UIColor.lightGray.cgColor
+        
+       // google.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
     }
 
-
 }
-
